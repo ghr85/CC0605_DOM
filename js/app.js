753921be32925a0568e33f1b1d93da8d5e57a17c
ghr85 <ghrenfrew@gmail.com>
2019-02-25 08:09:39 +0000

    added delete plants functionality

--- js/app.js
+++ js/app.js
@@ -1,77 +1,75 @@
 document.addEventListener('DOMContentLoaded', () => {
+const plantList = document.querySelector('#garden-list');
+const form = document.querySelector('#plant_input_form');
 
-const form = document.querySelector('#plant-input-form');
-form.addEventListener('submit', (event) => {
 
-  event.preventDefault();
 
-  const latinName = event.target.ltnnameinput.value;
-  const commonName = event.target.cmnnameinput.value;
-  const sunSelect = event.target.sunselect.value;
-  const soilSandy = event.target.soilsandy.value;
-  const soilLoam = event.target.soilloam.value;
-  const soilChalk = event.target.soilchalk.value;
-  const soilClay = event.target.soilclay.value;
-  const moistPoor = event.target.moistpoor.value;
-  const moistWell = event.target.moistwell.value;
-  const moistMoist = event.target.moistmoist.value;
+const handleDeleteAllClick = function (event) {
+  plantList.innerHTML = '';
+}
+const deleteAllButton = document.querySelector('#delete-all');
+deleteAllButton.addEventListener('click', handleDeleteAllClick);
 
-  const newContainer = document.createElement('div');
-  newContainer.classList.add('container');
 
-  const newCommonName = document.createElement('p');
-  newCommonName.textContent = commonName;
-  newCommonName.classList.add('common');
+  form.addEventListener('submit', (event) => {
 
-  const newLatinName = document.createElement('p');
-  newLatinName.textContent = latinName;
-  newLatinName.classList.add('latin');
+    event.preventDefault();
 
-  const newSunSelect = document.createElement('p');
-  newSunSelect.textContent = sunSelect;
-  newSunSelect.classList.add('sun');
+    const latinName = event.target.latin_name.value;
+    const commonName = event.target.common_name.value;
+    const sunSelect = event.target.sun_select.value;
 
-  const newSoilSandy = document.createElement('p');
-  newSoilSandy.textContent = soilSandy;
-  newSoilSandy.classList.add('soil');
 
-  const newSoilLoam = document.createElement('p');
-  newSoilLoam.textContent = soilLoam;
-  newSoilLoam.classList.add('soil');
 
-  const newSoilChalk = document.createElement('p');
-  newSoilChalk.textContent = soilChalk;
-  newSoilChalk.classList.add('soil');
+    const newContainer = document.createElement('div');
+    newContainer.classList.add('container');
 
-  const newSoilClay = document.createElement('p');
-  newSoilClay.textContent = soilClay;
-  newSoilClay.classList.add('soil');
+    const newCommonName = document.createElement('p');
+    newCommonName.textContent = commonName;
+    newCommonName.classList.add('common');
 
-  const newMoistPoor = document.createElement('p');
-  newMoistPoor.textContent = moistPoor;
-  newMoistPoor.classList.add('moist');
+    const newLatinName = document.createElement('p');
+    newLatinName.textContent = latinName;
+    newLatinName.classList.add('latin');
 
-  const newMoistWell = document.createElement('p');
-  newMoistWell.textContent = moistWell;
-  newMoistWell.classList.add('moist');
+    const newSunSelect = document.createElement('p');
+    newSunSelect.textContent = sunSelect;
+    newSunSelect.classList.add('sun');
 
-  const newMoistMoist = document.createElement('p');
-  newMoistMoist.textContent = moistMoist;
-  newMoistMoist.classList.add('moist');
 
-  const plantList = document.querySelector('#garden-list');
-  plantList.appendChild(newContainer);
 
-  newContainer.appendChild(newCommonName);
-  newContainer.appendChild(newLatinName);
-  newContainer.appendChild(newSunSelect);
-  newContainer.appendChild(newSoilSandy);
-  newContainer.appendChild(newSoilLoam);
-  newContainer.appendChild(newSoilChalk);
-  newContainer.appendChild(newSoilClay);
-  newContainer.appendChild(newMoistPoor);
-  newContainer.appendChild(newMoistWell);
-  newContainer.appendChild(newMoistMoist);
 
-});
+    plantList.appendChild(newContainer);
+
+    const soilSelectList = document.plant_input_form.soilSelect
+    const moistSelectList = document.plant_input_form.moistSelect
+
+    const getOptionals = function(options){
+      for (let i=0; i<options.length;i++){
+        if (options[i].checked === true){
+          const value = options[i].value; //assign a variable to the value
+          const group = options[i].name; //assign a variable to the name/class
+          const newEl = document.createElement('p'); //create a new p element
+          newEl.textContent = value;
+          newEl.classList.add(group);
+          newContainer.appendChild(newEl);
+        }
+      }
+
+    }
+
+
+    newContainer.appendChild(newCommonName);
+    newContainer.appendChild(newLatinName);
+    newContainer.appendChild(newSunSelect);
+    getOptionals(soilSelectList);
+    getOptionals(moistSelectList);
+
+
+
+  });
+
+
+
+
 });
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Codeclan Week 06 Day 05
+## Weekend Homework
+### Welcome to the DOM
+
+
+#### Order of Approach
+
+#### Notes
+
+* Big fucking notes: if you give radios/checkboxes the same name they are treated as an array
+* 
+
+#### Files
